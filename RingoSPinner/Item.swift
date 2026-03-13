86d50a38357b93e451d0cd394cc273855578f7ff
Fantom <ivanneporadnyj@gmail.com>
2026-03-13 14:13:35 +0200

    Initial Commit

---
+++ RingoSPinner/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  RingoSPinner
+//
+//  Created by Иван Непорадный on 13.03.2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ RingoSPinner/RingoSPinnerApp.swift
@@ -0,0 +1,32 @@
+//
+//  RingoSPinnerApp.swift
+//  RingoSPinner
+//
+//  Created by Иван Непорадный on 13.03.2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct RingoSPinnerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
